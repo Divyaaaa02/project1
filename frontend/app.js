b5f8e5a83322f0d6f8e53495ae7df5548950c2c8
Divyaaaa02 <143888030+Divyaaaa02@users.noreply.github.com>
2024-12-12 13:56:44 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DoctorList from './components/DoctorList';
+import BookingForm from './components/BookingForm';
+
+function App() {
+  return (
+    <div>
+      <DoctorList />
+      <BookingForm />
+    </div>
+  );
+}
+
+export default App;
